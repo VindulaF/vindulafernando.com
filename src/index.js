d6a4e028a46cac3319ed3d6513e52a86b5f1dd2e
VINDULAF-PC\Vindula Fernando <dinukav.fernando@gmail.com>
2017-08-01 00:07:05 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import JSONResume from './resume.json'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App resume={JSONResume} />, document.getElementById('root'))
+registerServiceWorker()
