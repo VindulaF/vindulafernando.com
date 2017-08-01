c280908eb1718cd57715b62984be659ad7b3eb29
VINDULAF-PC\Vindula Fernando <dinukav.fernando@gmail.com>
2017-08-01 23:04:32 +0530

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import JSONResume from './resume.json'
-import registerServiceWorker from './registerServiceWorker'
+//import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App resume={JSONResume} />, document.getElementById('root'))
-registerServiceWorker()
+//registerServiceWorker()
