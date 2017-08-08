024a5c8d7dfd3adff2d2d84a33a7468ddd8f426b
VINDULAF-PC\Vindula Fernando <dinukav.fernando@gmail.com>
2017-08-08 20:33:31 +0530

    Google analytics added.

--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ReactGA from 'react-ga'
+
 import Header from '../../components/Header'
 import About from '../../components/About'
 import WorkExperienceList from '../../components/WorkExperienceList'
@@ -7,23 +9,39 @@ import EducationList from '../../components/EducationList'
 import SkillList from '../../components/SkillList'
 import ReferenceList from '../../components/ReferenceList'
 
-const Resume = props =>
-	<div className="container">
-		<div className="row">
-			<div className="col-md-8 col-md-offset-2">
-				<Header basics={props.resume.basics} />
-				<hr />
-				<About basics={props.resume.basics} profiles={props.resume.profiles} />
-				<WorkExperienceList work={props.resume.work} />
-				<EducationList education={props.resume.education} />
-				<SkillList skills={props.resume.skills} />
-				<ReferenceList references={props.resume.references} />
-			</div>
-		</div>
-	</div>
+class Resume extends Component {
+	static propTypes = {
+		resume: PropTypes.object.isRequired
+	}
 
-Resume.propTypes = {
-	resume: PropTypes.object.isRequired
+	constructor(props) {
+		super(props)
+
+		ReactGA.initialize('UA-104247285-1')
+		ReactGA.set({ page: window.location.pathname })
+		ReactGA.pageview(window.location.pathname)
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-8 col-md-offset-2">
+						<Header basics={this.props.resume.basics} />
+						<hr />
+						<About
+							basics={this.props.resume.basics}
+							profiles={this.props.resume.profiles}
+						/>
+						<WorkExperienceList work={this.props.resume.work} />
+						<EducationList education={this.props.resume.education} />
+						<SkillList skills={this.props.resume.skills} />
+						<ReferenceList references={this.props.resume.references} />
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Resume
